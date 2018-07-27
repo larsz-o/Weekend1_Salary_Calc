4f81933d522ba3ff725ffd2aa3d8651fb7d79fa7
Lars Mackenzie <larsmackenzie@Larss-MacBook-Pro.local>
2018-07-27 16:42:46 -0500

    calculate and display monthly salary complete

--- client.js
+++ client.js
@@ -1,30 +1,35 @@
 
 $(document).ready(readyNow);
 
-const salaryArray = [];
+let salaryArray = [];
+let totalCosts = 0;
 
 function readyNow() {
-    $('#submitButton').on('click', appendEntries);
-    $('#submitButton').on('click', calculateCosts); 
+    $('#submitButton').on('click', appendDOM);
 }
 
-function appendEntries() {
-    let firstName = $('#firstNameInput').val();
-    let lastName = $('#lastNameInput').val();
-    let id = $('#idInput').val();
-    let title = $('#titleInput').val();
-    let annualSalary = $('#annualSalaryInput').val();
-    $('#employeeTableBody').append('<tr><td>' + firstName + '</td>' + '<td>' + lastName + '</td>' + '<td>' + id + '</td>' + '<td>' + title + '</td>' + '<td>' + annualSalary + '</td> </tr>');
-        salaryArray.push(annualSalary); // adds annualSalary to an array 
-    $('#firstNameInput').val('');
+function appendDOM() {
+    let firstName = $('#firstNameInput').val(); // gets value of first name input 
+    let lastName = $('#lastNameInput').val(); // gets value of last name input 
+    let id = $('#idInput').val(); // gets value of id input 
+    let title = $('#titleInput').val(); // gets value of title input 
+    let annualSalary = $('#annualSalaryInput').val(); // gets value of annual salary input 
+    $('#employeeTableBody').append('<tr><td>' + firstName + '</td>' + '<td>' + lastName + '</td>' + '<td>' + id + '</td>' + '<td>' + title + '</td>' + '<td>' + annualSalary + '</td> <td><button class="btn btn-info" id="delete-button">Delete Entry</button></td></tr>');
+    salaryArray.push(annualSalary); // adds annualSalary to an array 
+    $('#firstNameInput').val(''); 
     $('#lastNameInput').val('');
     $('#idInput').val('');
     $('#titleInput').val('');
     $('#annualSalaryInput').val('');
+    calculateCosts(salaryArray); 
 }
 
-// function calculateCosts(arrayOfSalaries){
-//     for (let i=0; i<arrayOfSalaries.length; i++){
+function calculateCosts(arrayOfSalaries) {
+    for (let i = 0; i < arrayOfSalaries.length; i++) {
+        arrayOfSalaries[i]= parseInt(arrayOfSalaries[i]);
+        totalCosts = (totalCosts + arrayOfSalaries[i]);
+   
+    }
+    $('#total-container').html('<p> Total Monthly Costs: $ ' + totalCosts + '</p>')
+}
 
-//     }
-// }
