ad06a1818fbc367267bad0373457399822ff771f
Lars <larsmackenzie@Larss-MacBook-Pro.local>
2018-07-29 11:05:56 -0500

    got totals to update upon delete

--- client.js
+++ client.js
@@ -1,8 +1,8 @@
 
 $(document).ready(readyNow);
 
-let salaryArray = [];
 let employeeArray = [];
+
 class Employee {
     constructor(firstName, lastName, id, title, annualSalary) {
         this.firstName = firstName;
@@ -15,6 +15,7 @@ class Employee {
 
 function readyNow() {
     $('#submitButton').on('click', appendDOM);
+    // $('#table-container').on('click', '#delete-button', deleteFromArray);
     $('#table-container').on('click', '#delete-button', deleteEntry);
 }
 
@@ -26,20 +27,19 @@ function appendDOM() {
     let annualSalaryInput = $('#annualSalaryInput').val(); // gets value of annual salary input
     let inputEmployee = new Employee(firstNameInput, lastNameInput, idInput, titleInput, annualSalaryInput);
     $('#employeeTableBody').append('<tr><td>' + firstNameInput + '</td>' + '<td>' + lastNameInput + '</td>' + '<td>' + idInput + '</td>' + '<td>' + titleInput + '</td>' + '<td id="salary-data"> $ ' + annualSalaryInput + '</td> <td><button class="btn btn-dark" id="delete-button">Delete Entry</button></td></tr>');
-    salaryArray.push(annualSalaryInput); // adds annualSalary to an array 
     employeeArray.push(inputEmployee); // adds employee to employeeArray
     $('#firstNameInput').val('');
     $('#lastNameInput').val('');
     $('#idInput').val('');
     $('#titleInput').val('');
     $('#annualSalaryInput').val('');
-    calculateCosts(salaryArray);
+    calculateCosts(employeeArray);
 }
 
-function calculateCosts(arrayOfSalaries) {
+function calculateCosts(arrayOfEmployees) {
     let monthlyTotal = 0;
-    for (let i = 0; i < arrayOfSalaries.length; i++) {
-        let numSalary = parseInt(arrayOfSalaries[i]);
+    for (let i = 0; i < arrayOfEmployees.length; i++) {
+        let numSalary = parseInt(arrayOfEmployees[i].annualSalary);
         let monthlySalary = numSalary / 12;
         console.log('Currently monthly salary is ', monthlySalary);
         monthlyTotal = monthlyTotal + (numSalary / 12);
@@ -50,22 +50,17 @@ function calculateCosts(arrayOfSalaries) {
         $('#total-container').css('background-color', '#d80614');
         $('#total-container').css('font-size', '1.5em');
         $('#total-container').css('font-weight', 'bold');
-        alert('Monthly salary expenses have gone over the limit of $20,000!')
     }
 }
 
 function deleteEntry() {
-    // let salaries = $(this).parent().parent().find('#salary-data);
-    // console.log(salaries);
-    // for (let i = 0; i < employeeArray.length; i++) {
-    //     console.log('in find salary data');
-    //     if (employeeArray[i] == salaries) {
-    //         employeeArray.splice[i, 1];
-    //         console.log(employeeArray);
-    //     }
-    // }
+    let index = $(this).closest('td').parent()[0].sectionRowIndex; //finds the index of the row that is deleted & assigns that number to "index"
+    console.log(index);
+    employeeArray.splice(index, 1);
+    console.log(employeeArray); 
     $(this).parent().parent().remove();
-    alert('Deleting an entry does not update the monthly total calculation. Sorry. Please reload the page to try again.');
-  
+    // alert('Deleting an entry does not update the monthly total calculation. Sorry. Please reload the page to try again.');
+    calculateCosts(employeeArray);
+
 }
 
